a57502345d77f222d3e40f401fae4479d50371ba
jonsoku2 <whdtjr2792@gmail.com>
2021-03-29 08:06:00 +0900

    save!

--- webpack5-ts-react/webpack.config.ts
+++ webpack5-ts-react/webpack.config.ts
@@ -9,7 +9,6 @@ import TerserPlugin from 'terser-webpack-plugin';
 import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
 import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
 import CopyPlugin from 'copy-webpack-plugin';
-import { merge } from 'webpack-merge';
 
 const isDevelopment = process.env.NODE_ENV !== 'production';
 
@@ -17,130 +16,6 @@ const PATHS = {
   src: path.join(__dirname, 'src'), //absolute path to RepoDir/src
   dist: path.join(__dirname, 'dist'), //absolute path to RepoDir/dist
   public: path.join(__dirname, 'public'), //absolute path to RepoDir/public
-};
-
-const commonConfig = {
-  resolve: {
-    extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-  entry: {
-    main: PATHS.src + '/index.tsx',
-  },
-  output: {
-    path: PATHS.dist,
-    filename: '[name].js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.css$/,
-        use: [
-          !isDevelopment ? MiniCssExtractPlugin.loader : 'style-loader', // 순서 2
-          'css-loader', // 순서 1
-        ],
-      },
-      {
-        test: /\.(png|jpg|gif|svg)$/,
-        loader: 'url-loader',
-        options: {
-          publicPath: './', // 빌드되었을때 path 들을 나타낼때...
-          name: '[name].[ext]?[hash]',
-          limit: 20000, // 20kb 미만은 data base64 로 변환 아니면? file-loader 로!
-        },
-      },
-    ],
-  },
-  plugins: [
-    new webpack.BannerPlugin({
-      banner: `
-                Build Date: ${new Date().toLocaleString()}
-                Commit Version: ${childProcess.execSync('git rev-parse --short HEAD')}
-                Author: ${childProcess.execSync('git config user.name')}
-            `,
-    }),
-    new webpack.DefinePlugin({
-      TWO: '1+1', // 1+1=2 가 나온다. 즉 값이다 ! 스트링아님
-      VALUE: JSON.stringify('1+1'), // 스트링으로 뽑고싶을때,
-      'api.domain': JSON.stringify('http://dev.api.domain.com'),
-    }),
-    new HtmlWebpackPlugin({
-      template: PATHS.public + '/index.html',
-      templateParameters: {
-        env: isDevelopment ? '(개발용)' : '',
-      },
-      minify: !isDevelopment
-        ? {
-            collapseWhitespace: true, // 빈칸을 제거
-            removeComments: true, // 주석을 제거
-          }
-        : false,
-    }),
-    new ForkTsCheckerWebpackPlugin({
-      async: false,
-      eslint: {
-        files: PATHS.src + '/**/*.{ts,tsx,js,jsx}', // required - same as command `eslint ./src/**/*.{ts,tsx,js,jsx} --ext .ts,.tsx,.js,.jsx`
-      },
-    }),
-    new CopyPlugin({
-      patterns: [
-        {
-          from: PATHS.public + '/assets',
-          to: 'assets', // 목적지 파일에 들어간다
-        },
-        // {
-        //   from: './node_modules/axios/dist/axios.min.js',
-        //   to: './axios.min.js', // 목적지 파일에 들어간다
-        // },
-      ],
-    }),
-  ],
-};
-
-const productionConfig = {
-  mode: 'production',
-  devtool: 'inline-source-map',
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: '[name].css',
-    }),
-    new CleanWebpackPlugin(),
-  ],
-  optimization: {
-    minimizer: [
-      new OptimizeCSSAssetsPlugin(),
-      new TerserPlugin({
-        terserOptions: {
-          compress: {
-            drop_console: true, // 콘솔 로그를 제거한다
-          },
-        },
-      }),
-    ],
-    splitChunks: {
-      chunks: 'all',
-    },
-  },
-};
-
-const developmentConfig = {
-  mode: 'development',
-  devtool: 'hidden-source-map',
-  devServer: {
-    overlay: true,
-    stats: 'errors-only',
-    hot: true,
-    historyApiFallback: true, // react router 새로고침해도 react-router 로 먹는다.
-    port: 3090, // front dev port
-  },
-  plugins: [new webpack.HotModuleReplacementPlugin(), new ReactRefreshWebpackPlugin()],
 };
 
 const config = {
@@ -176,7 +51,7 @@ const config = {
       {
         test: /\.css$/,
         use: [
-          !isDevelopment ? MiniCssExtractPlugin.loader : 'style-loader', // 순서 2
+          isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader, // 순서 2
           'css-loader', // 순서 1
         ],
       },
@@ -209,12 +84,12 @@ const config = {
       templateParameters: {
         env: isDevelopment ? '(개발용)' : '',
       },
-      minify: !isDevelopment
-        ? {
+      minify: isDevelopment
+        ? false
+        : {
             collapseWhitespace: true, // 빈칸을 제거
             removeComments: true, // 주석을 제거
-          }
-        : false,
+          },
     }),
     new ForkTsCheckerWebpackPlugin({
       async: false,
@@ -223,13 +98,13 @@ const config = {
       },
     }),
     new CleanWebpackPlugin(),
-    ...(!isDevelopment
-      ? [
+    ...(isDevelopment
+      ? [new webpack.HotModuleReplacementPlugin(), new ReactRefreshWebpackPlugin()]
+      : [
           new MiniCssExtractPlugin({
             filename: '[name].css',
           }),
-        ]
-      : [new webpack.HotModuleReplacementPlugin(), new ReactRefreshWebpackPlugin()]),
+        ]),
     new CopyPlugin({
       patterns: [
         {
@@ -244,8 +119,9 @@ const config = {
     }),
   ],
   optimization: {
-    minimizer: !isDevelopment
-      ? [
+    minimizer: isDevelopment
+      ? []
+      : [
           new OptimizeCSSAssetsPlugin(),
           new TerserPlugin({
             terserOptions: {
@@ -254,8 +130,7 @@ const config = {
               },
             },
           }),
-        ]
-      : [],
+        ],
     splitChunks: {
       chunks: 'all',
     },
